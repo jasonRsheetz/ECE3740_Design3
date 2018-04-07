346083ab73a60ab7ac4bf1a9edd81e3116e52063
Jason <10270672@my.uvu.edu>
2018-04-06 23:52:53 -0600

    code file now averages 8 4-bit numbers, average stops calculating after 8 numbers or if ADD is de-asserted. Further testing is needed to be sure all requirements are met and no errors can be found

--- Design3.v
+++ Design3.v
@@ -1,4 +1,38 @@
-module Design3(output out, input in);
+module Design3(output reg [3:0] AVE, input ADD, CLK, input [3:0] NUM);
+reg [31:0] total = 32'b0;
+integer recieved = 0;
+
+//recieve the new number on the falling edge of the clock
+always @(negedge CLK)begin
+
+if(ADD == 1'b1)begin
+	if(recieved < 8)begin
+	
+	total = total + NUM;
+	recieved = recieved + 1;
+	//end if
+	end
+	
+	 else if(recieved == 8)begin
+	
+	AVE = total/8;
+	recieved = 0;
+	//end if
+	end
+
+//end if
+end
+
+if(ADD == 1'b0)begin
+	
+	AVE = total/8;
+	recieved = 0;
+	
+	//end if
+	end
+	
+//end always
+end
 
 endmodule
-# ECE3740_Design3
+
