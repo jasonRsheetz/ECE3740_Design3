3c88c945f9021d55f38c17661d84e34f4515346d
Jason <10270672@my.uvu.edu>
2018-04-12 02:23:53 -0600

    final design

--- Design3.v
+++ Design3.v
@@ -1,6 +1,8 @@
 module Design3(output reg [3:0] AVE, input ADD, CLK, input [3:0] NUM);
 reg [31:0] total = 32'b0;
 integer recieved = 0;
+integer partial = 0;
+integer keep = 0;
 
 //recieve the new number on the falling edge of the clock
 always @(negedge CLK)begin
@@ -10,27 +12,32 @@ if(ADD == 1'b1)begin
 	
 	total = total + NUM;
 	recieved = recieved + 1;
+	partial = recieved;
 	//end if
 	end
 	
-	 else if(recieved == 8)begin
+	if(recieved == 8)begin
 	
 	AVE = total/8;
 	recieved = 0;
+	total = 0;
 	//end if
 	end
 
 //end if
 end
 
-if(ADD == 1'b0)begin
+if(ADD == 1'b0 & recieved == 8)begin
 	
-	AVE = total/8;
-	recieved = 0;
+	//AVE = total/8;
 	
 	//end if
-	end
-	
+end 
+else if(ADD == 1'b0 & recieved < 8)begin
+	AVE = total/partial;
+	recieved = 0;
+end
+
 //end always
 end
 
